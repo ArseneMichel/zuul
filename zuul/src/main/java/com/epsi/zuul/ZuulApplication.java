665057aa869d816302bb1a748e2f09df8394742d
ArseneMichel <arsene.michel@insead.edu>
2023-01-02 10:04:10 +0100

    Initial commit

---
+++ zuul/src/main/java/com/epsi/zuul/ZuulApplication.java
@@ -0,0 +1,26 @@
+package com.epsi.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@EnableZuulProxy
+@SpringBootApplication
+public class ZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulApplication.class, args);
+	}
+
+	@Bean
+	public UserDetailsService userDetailsService() throws Exception {
+		UserDetails user =User.withDefaultPasswordEncoder().username("user").password("password").roles("USER").build();
+
+		return new InMemoryUserDetailsManager(user);
+	}
+}
